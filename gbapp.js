806baf8c0e7e40070a48dfc3e2c30494cdf14825
upasna00 <S528126@mail.nwmissouri.edu>
2016-10-10 18:26:48 -0500

    m07

--- gbapp.js
+++ gbapp.js
@@ -7,9 +7,15 @@ var app = express();  // make express app
 var server = require('http').createServer(app);
 
 // set up the view engine
+app.set('views',path.resolve(__dirname,'views'));
+app.set('view engine','ejs');
 // manage our entries
 // set up the logger
 // GETS
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
 // POSTS
 // 404
 
